85e363b5fffc5de7e52b50098027fac7fa99856f
M_CODER <84375305+mcodersir@users.noreply.github.com>
2026-07-13 14:38:30 +0330

    Increment Android update code for rc.3

--- dicodePing_android/app/build.gradle.kts
+++ dicodePing_android/app/build.gradle.kts
@@ -81,7 +81,7 @@ android {
         applicationId = "ir.dicode.ping.client"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "0.1.3"
         multiDexEnabled = true
 
